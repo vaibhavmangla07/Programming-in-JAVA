f74e36537453d4436c13406b58cabcc2dffefae1
Vaibhav Mangla <135154345+vaibhavmangla07@users.noreply.github.com>
2024-05-25 19:18:21 +0530

    Add files via upload

---
+++ java002.java
@@ -0,0 +1,5 @@
+public class java002 {
+    public static void main(String[] args) {
+       System.out.println("PROGRAMMING\nIN\nJAVA");
+    }
+ }
---
+++ java004.java
@@ -0,0 +1,6 @@
+public class java004 {
+    public static void main(String []args) {
+        int sum = 10 + 20;
+        System.out.println(sum);
+    }
+}
