2435ef08e02c205ed6bb5f06f3227c3e9fa99f68
Vaibhav Mangla <135154345+vaibhavmangla07@users.noreply.github.com>
2024-07-08 11:37:36 +0530

    Add files via upload

---
+++ java005.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class java005 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Enter Name");
+        //INPUT
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        //OUTPUT
+        System.out.print("Your name : ");
+        System.out.println(name);
+    }
+}
